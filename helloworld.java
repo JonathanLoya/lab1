2072e1dd3580669ad4a61e48bd2438578479cbd1
Jonathan Loya <jonloya@nmsu.edu>
2017-01-26 14:58:29 -0700

    added hello world program... it's dialogue is quite dark...

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+// Jonathan Loya
+// 2017-01-26
+
+public class helloworld 
+{
+	public static void main( String args[] )
+	{
+		System.out.println("this is a pointless print statement for it conveys nothing...");
+		System.out.println("...");
+		System.out.print("shiver in eternal darkness\n");
+	}
+}
